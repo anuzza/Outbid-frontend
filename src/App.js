fde00de01fc8d12edd872c9199c089ba229bf37e
Anuja Sharma <asharm12@cs.olemiss.edu>
2022-09-26 13:20:25 -0500

    browser route setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import landing from "./containers/Landing/landing";
+import auth from "./containers/Auth/auth";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to OutBid</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" to="/landing" exact component={landing} />
+          <Route path="/landing" exact component={landing}></Route>
+          <Route path="/auth" exact component={auth}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
