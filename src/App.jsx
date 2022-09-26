d5e61bc076a08d45f4566af1184503cf56f310d8
Anuja Sharma <asharm12@cs.olemiss.edu>
2022-09-26 18:49:04 -0500

    added image slider in landing page

--- src/App.jsx
+++ src/App.jsx
@@ -17,3 +17,5 @@ function App() {
 }
 
 export default App;
+
+//#943d24
--- src/containers/Landing/landing.jsx
+++ src/containers/Landing/landing.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ImageSlider from "./Slide/ImageSlider";
+import { SliderData } from "./Slide/SliderData";
+import "./landing.css";
 
 const landing = () => {
-  return <div>landing</div>;
+  return (
+    <div className="wrapper">
+      <div className="slides">
+        <ImageSlider slides={SliderData} />
+      </div>
+    </div>
+  );
 };
 
 export default landing;
