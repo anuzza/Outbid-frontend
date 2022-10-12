0df03d032d09a3870f62256813c27452003fc8e1
jdtanner <jdtanner.go.olemiss.edu>
2022-10-12 17:52:27 -0500

    visual oppsie corrected

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const ItemCard = (props) => {
   return <Link to="/details">
-    <div className="card">{props.children}</div>;
+    <div className="card">{props.children}</div>
     </Link>
 };
 
