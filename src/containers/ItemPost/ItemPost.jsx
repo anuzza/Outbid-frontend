3c0779f3447c3157702e37f2c298825d00c460be
jdtanner <jdtanner.go.olemiss.edu>
2022-11-08 15:32:02 -0600

    finalized

--- src/containers/ItemPost/ItemPost.jsx
+++ src/containers/ItemPost/ItemPost.jsx
@@ -5,22 +5,10 @@ import { Redirect } from "react-router-dom";
 import BasicInfo from "./BasicInfo/Basic";
 import Upload from "./Image/Upload";
 import { useToasts } from "react-toast-notifications";
-<<<<<<< HEAD
-import useAuthStore from "../../store/auth";
-import CustomInputArea from "../../components/CustomInput/CustomInputArea";
-
-const ItemPost = () => {
-  const user = useAuthStore((state) => state.user);
-  const [classesName, setClasses] = useState({
-    classes: ["post"],
-  });
-  const { classes } = classesName;
-=======
 import Spinner from "../../components/Spinner/Spinner";
 
 const ItemPost = () => {
   const { addToast } = useToasts();
->>>>>>> main
 
   const [basicState, setBasicState] = useState({
     name: "",
@@ -40,10 +28,6 @@ const ItemPost = () => {
     setCondition((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-<<<<<<< HEAD
-  if (user) {
-    return <Redirect to="/auth" />;
-=======
   const changeBasicState = (e) => {
     return setBasicState((prev) => ({
       ...prev,
@@ -53,7 +37,6 @@ const ItemPost = () => {
 
   if (loading) {
     return <Spinner />;
->>>>>>> main
   }
 
   const checkErrors = () => {
@@ -74,76 +57,6 @@ const ItemPost = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div>
-      <p className="tip">
-      <h3>Create Your Listing Here! </h3>
-      </p>
-      <div className={classes.join(" ")}>
-        <div className="ItemPost">
-          <br/>
-          <form onSubmit={(e) => e}>
-            <CustomInput
-              onChange={(e) => handleFormChange(e)}
-              value={product_name}
-              type="product_name"
-              name="product_name"
-              required
-            >
-              Product Name
-            </CustomInput>
-            <CustomInput
-              onChange={(e) => handleFormChange(e)}
-              value={catergory}
-              type="catergory"
-              name="catergory"
-              required
-            >
-             Catergory
-            </CustomInput>
-            <CustomInput
-              onChange={(e) => handleFormChange(e)}
-              value={start_bid}
-              type="start_bid"
-              name="start_bid"
-              required
-            >
-              Starting Bid
-            </CustomInput>
-            <CustomInput
-              onChange={(e) => handleFormChange(e)}
-              value={condition}
-              type="condition"
-              name="condition"
-              required
-            >
-              Condition
-            </CustomInput>
-            <CustomInputArea
-              onChange={(e) => handleFormChange(e)}
-              value={details}
-              type="details"
-              name="details"
-              optional
-            >
-              Details
-            </CustomInputArea>
-            <CustomButton type="submit">
-              {loading ? (
-                <Spinner
-                  margin="2px auto"
-                  width="2em"
-                  height="2em"
-                  background="#13100a"
-                />
-              ) : (
-                "Post"
-              )}
-            </CustomButton>
-            <br/>
-          </form>
-        </div>
-=======
     <main role="main" className="main-container">
       <div className="main-wrap">
         <form onSubmit={(e) => handleFormSubmit(e)}>
@@ -203,7 +116,6 @@ const ItemPost = () => {
             List Item
           </CustomButton>
         </form>
->>>>>>> main
       </div>
     </main>
   );
